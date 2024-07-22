de8f106922f4e687e07f3690e3c776afef19e4fe
anthonychaussin <chaussin.anthony@gmail.com>
2024-07-22 19:03:08 +0200

    initial commit

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,9 @@
+import {base} from './base';
+
+export const environment = {
+  ENV: 'dev',
+  FIREBASE_CONFIG: base.FIRE,
+  FIREBASE_APPCHECK_DEBUG_TOKEN: base.DEBUG,
+  CATPCHA: base.CATPCHA,
+  VERSION: '0.0.1.1-alpha',
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+import {base} from './base';
+
+export const environment = {
+  ENV: 'staging',
+  FIREBASE_CONFIG: base.FIRE,
+  FIREBASE_APPCHECK_DEBUG_TOKEN: base.DEBUG,
+  CATPCHA: base.CATPCHA,
+  VERSION: '0.0.1.1-beta',
+}
